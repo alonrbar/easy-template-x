ac1472392fc796fe2b0b78a5db05da8880640c43
unknown <alon.r.bar@gmail.com>
2018-06-07 17:20:05 +0300

    loop plugin blueprint

--- src/plugins/loopPlugin.ts
+++ src/plugins/loopPlugin.ts
@@ -11,10 +11,33 @@ export class LoopPlugin extends TemplatePlugin {
     public containerTagReplacements(doc: Document, openTagIndex: number, closeTagIndex: number, allTags: Tag[], data: any): void {
 
         if (!data || !Array.isArray(data) || !data.length)
-            data = [{}];
+            data = [];
 
-        for (const item of data) {
-            console.log(item);
-        }
+        // first paragraph
+        // - get
+        // - clone
+        // - remove unnecessary parts
+
+        // last paragraph
+        // - get
+        // - clone
+        // - remove unnecessary parts
+
+        // middle paragraphs
+        // - get
+
+        // create dummy document
+        // compile to get new tags
+
+        // modify tags collection
+
+        // 1. re-merge first paragraphs
+        // 2. insert middle paragraphs
+        // 3. if (last iteration)
+        //      re-merge last paragraph
+        //    else
+        //      merge last clone with next first clone and go to (2)
+
+        // adjust compiler accordingly
     }
 }
