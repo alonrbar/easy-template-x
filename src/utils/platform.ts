720724a88b5540e3eb671c3968f8e84f6cea555b
unknown <alon.r.bar@gmail.com>
2018-06-12 16:38:41 +0300

    fix a bug in platform

--- src/utils/platform.ts
+++ src/utils/platform.ts
@@ -7,10 +7,14 @@ class Platform {
         if (this._isNode === null) {
 
             // https://stackoverflow.com/questions/4224606/how-to-check-whether-a-script-is-running-under-node-js
-            
-            const isNodeJS = (typeof process !== 'undefined') && ((process as any).release.name === 'node');
+
+            const isNodeJS = (
+                (typeof process !== 'undefined') &&
+                (typeof (process as any).release !== 'undefined') &&
+                (process as any).release.name === 'node'
+            );
             const isBrowser = (typeof window !== 'undefined');
-            
+
             this._isNode = (isNodeJS || !isBrowser);
         }
         return this._isNode;
