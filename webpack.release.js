c60f45acd9c0c8653e13219df41b8bb33150a089
unknown <alon.r.bar@gmail.com>
2019-05-06 22:55:41 +0300

    lint

--- webpack.release.js
+++ webpack.release.js
@@ -1,7 +1,6 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const base = require('./webpack.base.js');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(base, {
     mode: 'production',
