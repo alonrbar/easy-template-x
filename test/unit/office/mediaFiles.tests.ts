e3da6de6236a8e91ddee23f92f308decc423a24f
alonrbar <alon.r.bar@gmail.com>
2019-09-10 23:56:51 +0300

    test media hash

--- test/unit/office/mediaFiles.tests.ts
+++ test/unit/office/mediaFiles.tests.ts
@@ -23,5 +23,33 @@ describe(nameof(MediaFiles), () => {
             filesCount = await mediaFiles.count();
             expect(filesCount).toEqual(3);
         });
+
+        it('adds the same file only once', async () => {
+
+            const docFile = fs.readFileSync("./test/unit/res/two images.docx");
+            const zip = await JSZip.loadAsync(docFile);
+            const mediaFiles = new MediaFiles(zip);
+
+            let filesCount = await mediaFiles.count();
+            expect(filesCount).toEqual(2);
+
+            const mediaToAdd = "./test/unit/res/panda.jpg";
+
+            // add #1
+            const image1 = fs.readFileSync(mediaToAdd);
+            const addedFilePath1 = await mediaFiles.add(image1, MimeType.Jpeg);
+            expect(addedFilePath1).toEqual('word/media/media1.jpg');
+
+            filesCount = await mediaFiles.count();
+            expect(filesCount).toEqual(3);
+
+            // add #2
+            const image2 = fs.readFileSync(mediaToAdd);
+            const addedFilePath2 = await mediaFiles.add(image2, MimeType.Jpeg);
+            expect(addedFilePath2).toEqual('word/media/media1.jpg');
+
+            filesCount = await mediaFiles.count();
+            expect(filesCount).toEqual(3);
+        });
     });
 });
