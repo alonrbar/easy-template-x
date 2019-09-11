c81311ed92d1411dac62dfe4f368a0acbf3b700a
alonrbar <alon.r.bar@gmail.com>
2019-09-11 19:36:37 +0300

    add buffer issue fix from master

--- src/utils/binary.ts
+++ src/utils/binary.ts
@@ -11,7 +11,7 @@ export const Binary = {
 function toJsZipOutputType(binary: Binary): JSZip.OutputType;
 function toJsZipOutputType(binaryType: Constructor<Binary>): JSZip.OutputType;
 function toJsZipOutputType(binaryOrType: Binary | Constructor<Binary>): JSZip.OutputType {
-    
+
     if (!binaryOrType)
         throw new MissingArgumentError(nameof(binaryOrType));
 
@@ -27,6 +27,21 @@ function toJsZipOutputType(binaryOrType: Binary | Constructor<Binary>): JSZip.Ou
     if (typeof ArrayBuffer !== 'undefined' && inheritsFrom(binaryType, ArrayBuffer))
         return 'arraybuffer';
     if (typeof Buffer !== 'undefined' && inheritsFrom(binaryType, Buffer))
+        return 'nodebuffer';
+
+    //
+    // HACK: Fixes https://github.com/alonrbar/easy-template-x/issues/5  
+    //
+    // The issue happens due to some library on the way (maybe babel?)
+    // replacing Node's Buffer with this implementation:
+    // https://www.npmjs.com/package/buffer  
+    // The custom implementation actually uses an Uint8Array and therefor
+    // fails all the above `if` clauses.
+    //
+    // I'm assuming we'll be able to remove this if we create an esnext, non-compiled
+    // version with rollup or something of that sort.
+    //
+    if (typeof Uint8Array !== 'undefined' && inheritsFrom(binaryType, Uint8Array))
         return 'nodebuffer';
 
     throw new Error(`Binary type '${binaryType.name}' is not supported.`);
