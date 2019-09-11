9b62694053c9c71a0bf0305e5dbd384effeca309
alonrbar <alon.r.bar@gmail.com>
2019-09-11 19:27:13 +0300

    Binary detection hack - Fixes #5

--- src/utils/binary.ts
+++ src/utils/binary.ts
@@ -16,6 +16,21 @@ export const Binary = {
         if (typeof Buffer !== 'undefined' && binary instanceof Buffer)
             return 'nodebuffer';
 
+        //
+        // HACK: Fixes https://github.com/alonrbar/easy-template-x/issues/5  
+        //
+        // The issue happens due to some library on the way (maybe babel?)
+        // replacing Node's Buffer with this implementation:
+        // https://www.npmjs.com/package/buffer  
+        // The custom implementation actually uses an Uint8Array and therefor
+        // fails all the above `if` clauses.
+        //
+        // I'm assuming we'll be able to remove this if we create an esnext, non-compiled
+        // version with rollup or something of that sort.
+        //
+        if (typeof Uint8Array !== 'undefined' && binary instanceof Uint8Array)
+            return 'nodebuffer';
+
         throw new Error(`Binary type '${(binary as any).constructor.name}' is not supported.`);
     }
 };
