b6b02e14b71ec2fae5065078d5f9480042e0408e
alonrbar <alon.r.bar@gmail.com>
2019-10-18 15:56:57 +0300

    add 'no tags' tests

---
+++ test/fixtures/noTags.tests.ts
@@ -0,0 +1,30 @@
+import { TemplateHandler } from 'src/templateHandler';
+import { readFixture } from './fixtureUtils';
+
+describe('no tags fixture', () => {
+
+    it("does not throw on a template without tags", async () => {
+
+        const template = readFixture("no tags.docx");
+
+        const data = {};
+
+        const handler = new TemplateHandler();
+        await handler.process(template, data);
+    });
+
+    it("does not alter a document without tags", async () => {
+
+        const template = readFixture("no tags.docx");
+
+        const data = {};
+
+        const handler = new TemplateHandler();
+        const doc = await handler.process(template, data);
+
+        const docXml = await handler.getXml(doc);
+        expect(docXml).toMatchSnapshot();
+
+        // writeTempFile('no tags - output.docx', doc);
+    });
+});
