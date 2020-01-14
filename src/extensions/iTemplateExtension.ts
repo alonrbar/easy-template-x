73432d056621531fe884f20eff17edbdc5d3f0f6
Sebastian Rogers <rogerss@simpleinnovation.co.uk>
2020-01-14 14:59:07 +0000

    Changed casing of filename.

--- src/extensions/iTemplateExtension.ts
+++ src/extensions/iTemplateExtension.ts
@@ -1,9 +1,15 @@
-import { IExtensionUtilities } from './IExtensionUtilities';
-import { TemplateContext } from '../compilation';
-import { ScopeData } from '../compilation';
-import { XmlNode } from 'src';
+import { IExtensionUtilities } from "./IExtensionUtilities";
+import { TemplateContext } from "../compilation";
+import { ScopeData } from "../compilation";
+import { XmlNode } from "src";
 
 export interface ITemplateExtension {
-    getXmlDocuments(templateContext: TemplateContext): Promise<Map<string, XmlNode>>; 
-    execute(utilities: IExtensionUtilities, data: ScopeData, context: TemplateContext): void | Promise<void>;
+    getXmlDocuments(
+        templateContext: TemplateContext
+    ): Promise<Map<string, XmlNode>>;
+    execute(
+        utilities: IExtensionUtilities,
+        data: ScopeData,
+        context: TemplateContext
+    ): void | Promise<void>;
 }
