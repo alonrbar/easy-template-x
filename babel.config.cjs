92fb44061d7f8abce8a4e3ff82d8da67615efc28
alonrbar <alon.r.bar@gmail.com>
2024-08-12 19:03:16 +0300

    Simplify babel config

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,16 +1,13 @@
-const isJest = (process.env.NODE_ENV === 'test');
-
 module.exports = {
     "presets": [
+        ["@babel/preset-env", {
+            targets: {
+                chrome: 97 // https://caniuse.com/?search=es2022
+            }
+        }],
         "@babel/typescript"
     ],
     "plugins": [
-        "ts-nameof",
-        "@babel/proposal-class-properties",
-        "@babel/proposal-nullish-coalescing-operator",
-        "@babel/proposal-object-rest-spread",
-        "@babel/proposal-optional-catch-binding",
-        "@babel/proposal-optional-chaining",
-        isJest && '@babel/transform-modules-commonjs'
-    ].filter(Boolean)
+        "ts-nameof"
+    ]
 };
