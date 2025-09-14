38cf9f9f20f2a7c2d2bb1d6654554f0d087e7f55
alonrbar <alon.r.bar@gmail.com>
2025-09-14 17:48:10 +0300

    Debug vitest tests with source maps

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
+    plugins: [tsconfigPaths()],
     test: {
         environment: 'node',
         include: ['test/**/*.tests.ts'],
-        globals: true,
         snapshotSerializers: ['./test/xmlNodeSnapshotSerializer.ts'],
         reporters: process.env.GITHUB_ACTIONS ?
             ['default', 'junit', 'github-actions'] :
